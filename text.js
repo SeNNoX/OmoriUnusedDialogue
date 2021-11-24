a186de82a5ceca0c35270aa75b0d55ce314ed327
Senoquese <hugoliveira_mw@hotmail.com>
2021-11-24 03:26:01 -0100

    First commit

---
+++ text.js
@@ -0,0 +1,587 @@
+var t = {
+	"04_cutscenes-blackletters": [
+		{
+			"s": "HERO",
+			"h": "<span class=\"fOMORI_GAME\"><span class=\"quake\">W-Whoa, anyone else feel that?</span>",
+			"b": null,
+			"fs": "MainCharacters_DreamWorld",
+			"fi": 46
+		}, {
+			"s": "AUBREY",
+			"h": "<span class=\"fOMORI_GAME\"><span class=\"quake\">Yeah, everything feels fuzzy all of a sudden...</span>",
+			"b": null,
+			"fs": "MainCharacters_DreamWorld",
+			"fi": 7
+		}, {
+			"s": "KEL",
+			"h": "<span class=\"fOMORI_GAME\"><span class=\"quake\">Sure... feels... strange... around... here...</span>",
+			"b": null,
+			"fs": "MainCharacters_DreamWorld",
+			"fi": 31
+		}, {
+			"s": "KEL",
+			"h": "Whoa!! Everything's back to normal again!",
+			"b": null,
+			"fs": "MainCharacters_DreamWorld",
+			"fi": 19
+		}, {
+			"s": "HERO",
+			"h": "So many weird things happening lately...<br>We should stay close to each other in case anything happens.",
+			"b": null,
+			"fs": "MainCharacters_DreamWorld",
+			"fi": 40
+		}, {
+			"s": "AUBREY",
+			"h": "Sigh... I really hope BASIL is safe...",
+			"b": null,
+			"fs": "MainCharacters_DreamWorld",
+			"fi": 5
+		}, {
+			"s": "KEL",
+			"h": "<span class=\"fOMORI_GAME\"><span class=\"quake\">Oof... There's that fuzzy feeling again...</span>",
+			"b": null,
+			"fs": "MainCharacters_DreamWorld",
+			"fi": 28
+		}, {
+			"s": "HERO",
+			"h": "<span class=\"fOMORI_GAME\"><span class=\"quake\">Just hold on a bit longer...<br>We should... get used to it eventually...</span>",
+			"b": null,
+			"fs": "MainCharacters_DreamWorld",
+			"fi": 48
+		}, {
+			"s": "AUBREY",
+			"h": "<span class=\"fOMORI_GAME\"><span class=\"quake\">Look! That's the door that appeared right after BASIL vanished...</span>",
+			"b": null,
+			"fs": "MainCharacters_DreamWorld",
+			"fi": 4
+		}, {
+			"s": "AUBREY",
+			"h": "<span class=\"fOMORI_GAME\"><span class=\"quake\">Try opening the door, OMORI!</span>",
+			"b": null,
+			"fs": "MainCharacters_DreamWorld",
+			"fi": 4
+		}, {
+			"s": null,
+			"h": "The door won't budge.",
+			"b": null,
+			"fs": null,
+			"fi": null
+		}, {
+			"s": "KEL",
+			"h": "<span class=\"fOMORI_GAME\">Darn... I guess it's locked.",
+			"b": null,
+			"fs": "MainCharacters_DreamWorld",
+			"fi": 27
+		}, {
+			"s": "AUBREY",
+			"h": "<span class=\"fOMORI_GAME\">Where do you think it goes to?<br>Do you think BASIL is in there?",
+			"b": null,
+			"fs": "MainCharacters_DreamWorld",
+			"fi": 4
+		}, {
+			"s": "HERO",
+			"h": "<span class=\"fOMORI_GAME\">Possibly... Although, I have a feeling that wherever it leads to will be dangerous...",
+			"b": null,
+			"fs": "MainCharacters_DreamWorld",
+			"fi": 40
+		}, {
+			"s": "AUBREY",
+			"h": "<span class=\"fOMORI_GAME\">But BASIL is our friend! We have to do something...",
+			"b": null,
+			"fs": "MainCharacters_DreamWorld",
+			"fi": 5
+		}, {
+			"s": "KEL",
+			"h": "<span class=\"fOMORI_GAME\">Move over, OMORI! I'm goin' in!",
+			"b": null,
+			"fs": "MainCharacters_DreamWorld",
+			"fi": 30
+		}, {
+			"s": "KEL",
+			"h": "<span class=\"fOMORI_GAME\"><span class=\"quake\"><span class=\"s5\">BASIL!! I'm coming to save you!!",
+			"b": null,
+			"fs": "MainCharacters_DreamWorld",
+			"fi": 30
+		}, {
+			"s": "HERO",
+			"h": "<span class=\"fOMORI_GAME\"><span class=\"s5\">Whoa,! Are you okay!?",
+			"b": null,
+			"fs": "MainCharacters_DreamWorld",
+			"fi": 46
+		}, {
+			"s": "AUBREY",
+			"h": "<span class=\"fOMORI_GAME\"><span class=\"s5\">What are you doing, you dummy!?",
+			"b": null,
+			"fs": "MainCharacters_DreamWorld",
+			"fi": 10
+		}, {
+			"s": "KEL",
+			"h": "<span class=\"fOMORI_GAME\"><span class=\"quake\">I don't know! I'm not equipped to handle difficult situations!",
+			"b": null,
+			"fs": "MainCharacters_DreamWorld",
+			"fi": 33
+		}, {
+			"s": "KEL",
+			"h": "<span class=\"fOMORI_GAME\"><span class =\"s5\"><span class=\"quake\">Ahhhhhhh!! What was that!?</span>",
+			"b": null,
+			"fs": "MainCharacters_DreamWorld",
+			"fi": 31
+		}, {
+			"s": "HERO",
+			"h": "Hmmm... It looks like a map of the forest...<br>but some parts of it are blacked out...",
+			"b": null,
+			"fs": "MainCharacters_DreamWorld",
+			"fi": 39
+		}, {
+			"s": "KEL",
+			"h": "What's that on the back?",
+			"b": null,
+			"fs": "MainCharacters_DreamWorld",
+			"fi": 19
+		}, {
+			"s": "KEL",
+			"h": "H... A... N... G... M... A... N...?",
+			"b": null,
+			"fs": "MainCharacters_DreamWorld",
+			"fi": 29
+		}, {
+			"s": "HERO",
+			"h": "Oh hey, I know this game!<br>We've probably even played it once or twice before...",
+			"b": null,
+			"fs": "MainCharacters_DreamWorld",
+			"fi": 39
+		}, {
+			"s": "HERO",
+			"h": "In HANGMAN, you choose different letters to complete a message.",
+			"b": null,
+			"fs": "MainCharacters_DreamWorld",
+			"fi": 36
+		}, {
+			"s": "HERO",
+			"h": "If you choose a correct letter, the letter is used to filled in the blanks... but if you get a letter wrong, a part of a hangman is drawn.",
+			"b": null,
+			"fs": "MainCharacters_DreamWorld",
+			"fi": 39
+		}, {
+			"s": "HERO",
+			"h": "To win, you just have to guess the correct letters to complete the message...",
+			"b": null,
+			"fs": "MainCharacters_DreamWorld",
+			"fi": 36
+		}, {
+			"s": "HERO",
+			"h": "But if you choose too many wrong letters and the hangman drawing is completed... you lose the game.",
+			"b": null,
+			"fs": "MainCharacters_DreamWorld",
+			"fi": 42
+		}, {
+			"s": "KEL",
+			"h": "Wow... That doesn't sound fun at all...<br>Why is there even a game like that?",
+			"b": null,
+			"fs": "MainCharacters_DreamWorld",
+			"fi": 24
+		}, {
+			"s": "HERO",
+			"h": "Hey, look! That big letter “A” we found earlier is filling in some of the blanks!",
+			"b": null,
+			"fs": "MainCharacters_DreamWorld",
+			"fi": 39
+		}, {
+			"s": "HERO",
+			"h": "If we're able to complete this message... I'm sure we'll be able to open the door and save BASIL!",
+			"b": null,
+			"fs": "MainCharacters_DreamWorld",
+			"fi": 43
+		}, {
+			"s": "AUBREY",
+			"h": "There must be more MISSING LETTERS around the forest!<br>We should hurry and go look for them.",
+			"b": null,
+			"fs": "MainCharacters_DreamWorld",
+			"fi": 10
+		}, {
+			"s": "AUBREY",
+			"h": "I'm... I'm worried about BASIL... <br>He doesn't... do well on his own...",
+			"b": null,
+			"fs": "MainCharacters_DreamWorld",
+			"fi": 5
+		}, {
+			"s": "KEL",
+			"h": "Don't worry, AUBREY!<br>BASIL might be a little on the softer side, but he knows how to take care of himself.",
+			"b": null,
+			"fs": "MainCharacters_DreamWorld",
+			"fi": 30
+		}, {
+			"s": "AUBREY",
+			"h": "Yeah, I guess you're right.<br>We just need to believe in him...",
+			"b": null,
+			"fs": "MainCharacters_DreamWorld",
+			"fi": 4,
+		}
+	],
+	"25_cutscenes_blackhole": [
+		{
+			"s": null,
+			"h": "The black hole sucks in all letters, and becomes a black door.<br><br>Screen becomes in very glitchy.",
+			"b": null,
+			"fs": null,
+			"fi": null
+		}, {
+			"s": "AUBREY",
+			"h": "<span class =\"s5\"><span class=\"quake\"><OMORI_GAME>Ahhhhhhh... the door... it's open...",
+			"b": null,
+			"fs": "MainCharacters_DreamWorld",
+			"fi": 7
+		}, {
+			"s": "HERO",
+			"h": "<span class =\"s5\"><span class=\"quake\"><OMORI_GAME>... This... This isn't looking good...",
+			"b": null,
+			"fs": "MainCharacters_DreamWorld",
+			"fi": 48
+		}, {
+			"s": "KEL",
+			"h": "<span class =\"s5\"><span class=\"quake\"><OMORI_GAME>BASIL... BASIL must be somewhere inside!",
+			"b": null,
+			"fs": "MainCharacters_DreamWorld",
+			"fi": 30
+		}, {
+			"s": "KEL",
+			"h": "<span class=\"fOMORI_GAME\"><span class=\"quake\">GAAAAAAAAHHHHHHHHHHHHH!!!",
+			"b": null,
+			"fs": "MainCharacters_DreamWorld",
+			"fi": 34
+		}, {
+			"s": "AUBREY",
+			"h": "<span class=\"quake\"><OMORI_GAME>KEL! Are you okay!?",
+			"b": null,
+			"fs": "MainCharacters_DreamWorld",
+			"fi": 7
+		}, {
+			"s": "AUBREY",
+			"h": "<span class=\"quake\"><OMORI_GAME>Let... Let me try!",
+			"b": null,
+			"fs": "MainCharacters_DreamWorld",
+			"fi": 7
+		}, {
+			"s": "AUBREY",
+			"h": "<span class=\"fOMORI_GAME\"><span class=\"quake\">AAAAAAAAHHHHHHHHHHHHHHHHHHH!!!",
+			"b": null,
+			"fs": "MainCharacters_DreamWorld",
+			"fi": 13
+		}, {
+			"s": "HERO",
+			"h": "<span class =\"s5\">KEL! AUBREY! Stop! It's no use...<br>... I... I don't think we're going to be able do this...",
+			"b": null,
+			"fs": "MainCharacters_DreamWorld",
+			"fi": 48
+		}, {
+			"s": "KEL",
+			"h": "Huh!? W-What do you mean, HERO?<br>How else are we going to save BASIL!?",
+			"b": null,
+			"fs": "MainCharacters_DreamWorld",
+			"fi": 20
+		}, {
+			"s": "HERO",
+			"h": "Well...",
+			"b": null,
+			"fs": "MainCharacters_DreamWorld",
+			"fi": 48
+		}, {
+			"s": "HERO",
+			"h": "We won't be able to save him... but OMORI can.",
+			"b": null,
+			"fs": "MainCharacters_DreamWorld",
+			"fi": 44
+		}, {
+			"s": "HERO",
+			"h": "OMORI... I was hoping we could do this together, but...<br>I've known since the beginning... that you've been the only one who has been able to touch the MISSING LETTERS...",
+			"b": null,
+			"fs": "MainCharacters_DreamWorld",
+			"fi": 43
+		}, {
+			"s": "KEL",
+			"h": "<span class =\"s5\">What are you saying, HERO!? We can't leave him alone.<br>We've done everything together!",
+			"b": null,
+			"fs": "MainCharacters_DreamWorld",
+			"fi": 30
+		}, {
+			"s": "AUBREY",
+			"h": "<span class =\"s5\">You're going to let OMORI go in there all by himself!?<br>How will he be able to find BASIL without us?",
+			"b": null,
+			"fs": "MainCharacters_DreamWorld",
+			"fi": 5
+		}, {
+			"s": "HERO",
+			"h": "... I believe that OMORI is strong enough to do this... And... <br>I know he wants to save BASIL more than anyone...",
+			"b": null,
+			"fs": "MainCharacters_DreamWorld",
+			"fi": 43
+		}, {
+			"s": "HERO",
+			"h": "If it's going to be like this... If we all go... I don't think we won't be able to help at all...<br>It... It would be better if OMORI goes by himself...",
+			"b": null,
+			"fs": "MainCharacters_DreamWorld",
+			"fi": 40
+		}, {
+			"s": "AUBREY",
+			"h": "But... we must be able to help somehow...",
+			"b": null,
+			"fs": "MainCharacters_DreamWorld",
+			"fi": 5
+		}, {
+			"s": "HERO",
+			"h": "Hmm... Well, it's not much, but... even if we can't be there fighting next to him... the least we can do is guide his way back home...",
+			"b": null,
+			"fs": "MainCharacters_DreamWorld",
+			"fi": 40
+		}, {
+			"s": "HERO",
+			"h": "OMORI... I don't know what's in there... but if you're ever get lost or scared... just follow our voices back here!<br>You should know... that you'll always have us!",
+			"b": null,
+			"fs": "MainCharacters_DreamWorld",
+			"fi": 39
+		}, {
+			"s": "KEL",
+			"h": "... OMORI... I know you don't like to be alone... but... it's just like HERO said... We'll just get in the way... Stay strong, okay!? And know that we'll always have your back!",
+			"b": null,
+			"fs": "MainCharacters_DreamWorld",
+			"fi": 20
+		}, {
+			"s": "AUBREY",
+			"h": "Yeah... We believe in you, OMORI. We always have!<br>Please be safe... We'll always be waiting for you here...<br>no matter what!",
+			"b": null,
+			"fs": "MainCharacters_DreamWorld",
+			"fi": 5
+		}, {
+			"s": "AUBREY",
+			"h": "Goodbye, OMORI... See you soon...",
+			"b": null,
+			"fs": "MainCharacters_DreamWorld",
+			"fi": 4
+		}, {
+			"s": "HERO",
+			"h": "We can only help you get this far... but from now on...<br>it looks like you're going to have to go at it alone.",
+			"b": null,
+			"fs": "MainCharacters_DreamWorld",
+			"fi": 48
+		}, {
+			"s": "BASIL",
+			"h": "<span class=\"fOMORI_GAME\">Ah... OMORI... It's you... You're finally here... </span>I knew you'd come eventually...",
+			"b": null,
+			"fs": "MainCharacter_Basil",
+			"fi": 1
+		}, {
+			"s": "BASIL",
+			"h": "<span class=\"fOMORI_GAME\">I'm sorry I'm so useless... I hope we can find a way out of here...",
+			"b": null,
+			"fs": "MainCharacter_Basil",
+			"fi": 4
+		}, {
+			"s": null,
+			"h": "OMORI and BASIL walk out of Black Door.<br><br>Black Door disappears.",
+			"b": null,
+			"fs": null,
+			"fi": null
+		}, {
+			"s": "KEL",
+			"h": "<span class =\"s5\">Holy moly! It's OMORI and BASIL!<br>You did it, OMORI! You really did it!<br>",
+			"b": null,
+			"fs": "MainCharacters_DreamWorld",
+			"fi": 18
+		}, {
+			"s": "AUBREY",
+			"h": "Oh, I'm so relieved...",
+			"b": null,
+			"fs": "MainCharacters_DreamWorld",
+			"fi": 4
+		}, {
+			"s": "KEL",
+			"h": "I don't really wanna admit it, but I was starting to get a little worried, heh heh...",
+			"b": null,
+			"fs": "MainCharacters_DreamWorld",
+			"fi": 17
+		}, {
+			"s": "AUBREY",
+			"h": "BASIL, BASIL! You're finally back!<br>How are you feeling? Are you okay?",
+			"b": null,
+			"fs": "MainCharacters_DreamWorld",
+			"fi": 1
+		}, {
+			"s": "BASIL",
+			"h": "I'm fine... I think. Sorry that I made you worry...",
+			"b": null,
+			"fs": "MainCharacter_Basil",
+			"fi": 1
+		}, {
+			"s": "AUBREY",
+			"h": "Oh, don't apologize, BASIL!<br>You're safe and that's what matters.",
+			"b": null,
+			"fs": "MainCharacters_DreamWorld",
+			"fi": 2
+		}, {
+			"s": "HERO",
+			"h": "Let's go see MARI!<br>I'm sure she'll be so excited to hear the news!<br>Lead the way, OMORI!",
+			"b": null,
+			"fs": "MainCharacters_DreamWorld",
+			"fi": 37
+		}
+	],
+	"farawaytown_extradialogue": [
+		{
+			"s": null,
+			"h": "Even though KEL is just as you remember him...<br>you still can tell he has grown while you were gone.<br>Something about that hurts, but you don't know why.",
+			"b": null,
+			"fs": null,
+			"fi": null,
+		}, {
+			"s": null,
+			"h": "Loud, angry, violent... AUBREY has changed a lot...<br>But, she still hardly makes a noise when she sleeps.",
+			"b": null,
+			"fs": null,
+			"fi": null,
+		}, {
+			"s": null,
+			"h": "HERO looks peaceful... but you can't help but worry.<br>For a moment, you wonder what his life would have been like if MARI was still there.",
+			"b": null,
+			"fs": null,
+			"fi": null,
+		}
+	],
+	"party_dialogue": [
+		{
+			"s": null,
+			"h": "Entering Basil's House: (Void, giant black hole covering almost all of the floor)<br><br>*Upon entering, OMORI walks alone and his party members block the entrance. STAB becomes available as an option*<br><br>*A bunch of eyes open up in the darkness and close again*",
+			"b": null,
+			"fs": null,
+			"fi": null
+		}, {
+			"s": "HERO",
+			"h": "Whoa... What is this place? It's a mess...<br>Who do you think it belonged to?",
+			"b": null,
+			"fs": "MainCharacters_DreamWorld",
+			"fi": 46
+		}, {
+			"s": "AUBREY",
+			"h": "Watch where you're stepping KEL!<br>There isn't a floor here...",
+			"b": null,
+			"fs": "MainCharacters_DreamWorld",
+			"fi": 10
+		}, {
+			"s": "KEL",
+			"h": "It doesn't look like there are any ghosts, either.<br><span class=\"sinv\">Maaaaaaannnnnnnnnnnnn...</span> What a letdown!",
+			"b": null,
+			"fs": "MainCharacters_DreamWorld",
+			"fi": 26
+		}, {
+			"s": "MARI",
+			"h": "I don't know why, but I can't help but feel a little depressed in this place...",
+			"b": null,
+			"fs": "MainCharacter_Mari",
+			"fi": 4
+		}, {
+			"s": "MARI",
+			"h": "Sigh... I guess there really is nothing here after all.",
+			"b": null,
+			"fs": "MainCharacter_Mari",
+			"fi": 4
+		}, {
+			"s": null,
+			"h": "It's a long way down... Do you want to jump?",
+			"b": null,
+			"fs": null,
+			"fi": null
+		}, {
+			"s": "MARI",
+			"h": "OMORI! What are you doing?<br>You look like you were just about to fall in!",
+			"b": null,
+			"fs": "MainCharacter_Mari",
+			"fi": 2
+		}, {
+			"s": "MARI",
+			"h": "Stay with me, OMORI... I don't want to lose you again.",
+			"b": null,
+			"fs": "MainCharacter_Mari",
+			"fi": 4
+		}, {
+			"s": null,
+			"h": "*OMORI must stab himself in front of his friends.<br><br>*While OMORI is stabbing, his frames switch between OMORI and PLAYER's.<br><br>*After he does so, PLAYER (not OMORI) wakes up in BASIL's house alone.<br><br>*His friends are frozen in time and it's glitchy.<br><br>*PLAYER can fall into the hole now that he's alone.",
+			"b": null,
+			"fs": null,
+			"fi": null
+		}, {
+			"s": null,
+			"h": "It's a long way down...",
+			"b": null,
+			"fs": null,
+			"fi": null
+		}, {
+			"s": null,
+			"h": "*PLAYER falls down the hole animation.<br><br>*PLAYER falls in MARI's coffin map in a pool and must “face his fears” by swimming up from the pool to a forest with spider webs, then climbing a staircase.<br><br>*One PLAYER reaches the top of the staircase, camera pans down from top to showcase MARI's coffin before centering on PLAYER.<br><br>*When PLAYER tries to reach the coffin, PLAYER jolts back and the screen glitches out. During the glitch, PLAYER's sprite turns into OMORI's while MARI's coffin also suddenly disappears. In place of MARI's coffin is a black door.",
+			"b": null,
+			"fs": null,
+			"fi": null
+		}, {
+			"s": null,
+			"h": "A black door casts no shadow. What do you want to do?",
+			"b": null,
+			"fs": null,
+			"fi": null
+		}
+	],
+	"24_cutscenes_finalboss": [
+		{
+			"s": "AUBREY",
+			"h": "Wherever BASIL is... he's probably all alone.",
+			"b": null,
+			"fs": "MainCharacters_DreamWorld",
+			"fi": 5
+		}, {
+			"s": "AUBREY",
+			"h": "We need to hurry and find him! I bet he could use a big hug right now!",
+			"b": null,
+			"fs": "MainCharacters_DreamWorld",
+			"fi": 5
+		}, {
+			"s": "KEL",
+			"h": "Yeah, I bet he could! When we're all together again, we should all give him a big ol' hug! Heh, heh!",
+			"b": null,
+			"fs": "MainCharacters_DreamWorld",
+			"fi": 18
+		}, {
+			"s": "HERO",
+			"h": "Aww...<br>It's nice to see you two getting along so well for a change.<br>I could hug both of you right now!",
+			"b": null,
+			"fs": "MainCharacters_DreamWorld",
+			"fi": 37
+		}, {
+			"s": "AUBREY",
+			"h": "H-Huh!? Me and KEL... G-Get along?",
+			"b": null,
+			"fs": "MainCharacters_DreamWorld",
+			"fi": 6
+		}, {
+			"s": "KEL",
+			"h": "W-What are you talking about, HERO? Stop being weird!",
+			"b": null,
+			"fs": "MainCharacters_DreamWorld",
+			"fi": 30
+		}, {
+			"s": "HERO",
+			"h": "Haha... Relax you two. I'm just playing around...",
+			"b": null,
+			"fs": "MainCharacters_DreamWorld",
+			"fi": 38
+		}, {
+			"s": "HERO",
+			"h": "Hold on, BASIL... We're almost there. I can feel it.",
+			"b": null,
+			"fs": "MainCharacters_DreamWorld",
+			"fi": 36
+		}, {
+			"s": "HERO",
+			"h": "We'll all be together soon...",
+			"b": null,
+			"fs": "MainCharacters_DreamWorld",
+			"fi": 36
+		}
+	]
+}
